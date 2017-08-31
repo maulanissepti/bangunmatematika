7ccd90f18e9c4014adfdf1383dd35c29010fdf66
maulanissepti <maulanissepty@gmail.com>
2017-08-31 19:59:33 +0700

    shuffle func

--- www/js/soalpersegi1.js
+++ www/js/soalpersegi1.js
@@ -63,3 +63,8 @@ function resetSoal(jsoal,is){
 	}
 	//location.reload();
 }
+
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
